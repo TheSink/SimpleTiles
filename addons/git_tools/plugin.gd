80bba962f4aa0785fe7db645e2e6959e0b00478a
TheSink <lucianedean@gmail.com>
2020-05-01 12:32:51 -0700

    Add push/pull command plugin

---
+++ addons/git_tools/plugin.gd
@@ -0,0 +1,28 @@
+tool
+extends EditorPlugin
+
+var output = ["none"]
+
+
+func _enter_tree():
+	add_tool_menu_item("[Git] Pull From Repo", self, "pull")
+	add_tool_menu_item("[Git] Push To Repo", self, "push")
+
+
+func _exit_tree():
+	remove_tool_menu_item("[Git] Pull From Repo")
+	remove_tool_menu_item("[Git] Push To Repo")
+
+func pull(_arg):
+	OS.execute("git", ["pull"], true, output)
+	output_log()
+
+func push(_arg):
+	OS.execute("git", ["push"], true, output)
+	output_log()
+	
+func output_log():
+	var string = ""
+	for element in output:
+		string += element + "\n"
+	print(string)
