909fbbe93a94cdeecd79d42d17815b15230bf56b
TheSink <lucianedean@gmail.com>
2020-05-01 12:34:59 -0700

    Update git tools

--- addons/git_tools/plugin.gd
+++ addons/git_tools/plugin.gd
@@ -14,10 +14,12 @@ func _exit_tree():
 	remove_tool_menu_item("[Git] Push To Repo")
 
 func pull(_arg):
+	output = []
 	OS.execute("git", ["pull"], true, output)
 	output_log()
 
 func push(_arg):
+	output = []
 	OS.execute("git", ["push"], true, output)
 	output_log()
 	
